850e2de8f4caf4695d659450194bcc667223591f
Oğuzhan <ogzcakar@gmail.com>
2017-01-11 04:54:59 +0000

    Update index.php

--- index.php
+++ index.php
@@ -19,7 +19,7 @@ $arabamCom  = new arabamCom();
     //Klasik Araçlar Kategorisinin 2.sayfasındaki ilanlarını listeler
 
 //print_r($arabamCom->categoryListContent('Klasik Araçlar' , 'Cadillac ' , '2' ));
-    //Klasik Araçlar Menüsünün Cadillac Alt Kategorisinin 2.sayfasındaki ilanları listeler
+    //Klasik Araçlar Kategorisinin Cadillac Alt Kategorisinin 2.sayfasındaki ilanları listeler
 
 //print_r($arabamCom->detail('http://www.arabam.com/ilan/sahibinden-satilik-mercedes-180/2011-mercedes-c-180-1-8-7-ileri-wurth-seramik-kaplamali/6439322'));
     //Girilen İlan Linkinin detayını listeler
